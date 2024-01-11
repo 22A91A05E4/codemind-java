7b9ecb9328b4588893478cb6ac38d00c44714bad
22A91A05E4 <119839946+22A91A05E4@users.noreply.github.com>
2024-01-11 11:58:58 +0530

    import java.util.*; public class Ap {     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         int x = sc.nextInt();         int a = x*x;         int p = 4*x;         System.out.print(a+" ");         System.out.print(p);     } }

---
+++ Area_and_Perimeter_of_Square.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Ap
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int a = x*x;
+        int p = 4*x;
+        System.out.print(a+" ");
+        System.out.print(p);
+    }
+}
