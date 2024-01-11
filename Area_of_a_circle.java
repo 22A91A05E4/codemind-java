49292f7453396c38aa92a1bce0cdbdbf4150ab00
22A91A05E4 <119839946+22A91A05E4@users.noreply.github.com>
2024-01-11 11:34:10 +0530

    import java.util.*; // import java.util.*; public class Cir {     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         int a = sc.nextInt();         double x = 3.14*a*a;         System.out.printf("%.2f",x);     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,12 @@
+import java.util.*;
+// import java.util.*;
+public class Cir
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        double x = 3.14*a*a;
+        System.out.printf("%.2f",x);
+    }
+}
