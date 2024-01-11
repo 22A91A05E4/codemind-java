6e0a26749c1bb73d953c92b60ee577a211bf0638
22A91A05E4 <119839946+22A91A05E4@users.noreply.github.com>
2024-01-11 11:51:19 +0530

    import java.util.*; public class Vol {     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         int x = sc.nextInt();         double a = 1.333333*3.14*x*x*x;         System.out.printf("%.2f",a);     } }

---
+++ Volume_of_Sphere.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Vol
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        double a = 1.333333*3.14*x*x*x;
+        System.out.printf("%.2f",a);
+    }
+}
